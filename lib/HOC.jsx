2ead9a92cb8d0096cb550387ad3f49e52ec07832
Eric Hou <erichou2004@gmail.com>
2017-06-24 18:30:45 -0700

    First round HOC

--- lib/HOC.jsx
+++ lib/HOC.jsx
@@ -1,12 +1,25 @@
 import {Component} from 'react';
 
+let bindMethodNames = ['onMouseDown', 'onMouseUp', 'onMouseEnter', 'onMouseLeave',
+                       'onMouseClick', 'className']
+
 class HOC extends Component {
   constructor(props) {
     super(props);
+    let bindMethodNames = Object.getOwnPropertyNames(this.prototype);
+    for (var methodName in bindMethodNames) {
+      if (typeof methodName == 'function') {
+        this[methodName] = this[methodName].bind(this);
+      }
+    }
   }
-
-
-
-
-
 }
+
+for (var methodName in bindMethodNames) {
+  HOC[methodName] = function(args) {
+    this.props[methodName] ? this.props[methodName](...args) : null;
+  }
+}
+
+export default HOC;
+export {HOC};

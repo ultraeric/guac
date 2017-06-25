670b0920dc99bd99d0ee00837f60d0d916fa93a2
Eric Hou <erichou2004@gmail.com>
2017-06-24 20:01:18 -0700

    sync

--- lib/HOC.jsx
+++ lib/HOC.jsx
@@ -1,24 +1,24 @@
 import {Component} from 'react';
 
 let bindMethodNames = ['onMouseDown', 'onMouseUp', 'onMouseEnter', 'onMouseLeave',
-                       'onMouseClick', 'className']
+                       'onMouseClick', 'className'];
 
-class HOC extends Component {
-  constructor(props) {
-    super(props);
-    let bindMethodNames = Object.getOwnPropertyNames(this.prototype);
-    for (var methodName in bindMethodNames) {
-      if (typeof methodName == 'function') {
-        this[methodName] = this[methodName].bind(this);
-      }
+//Wrap your higher-ordered components with this
+function HOC(WrappedComponent) {
+  for (var methodName in bindMethodNames) {
+
+    //Closure to keep ownMethod reference
+    function composeFunction(methodName) {
+      let ownMethod = WrappedComponent[methodName];
+      return function(args) {
+        ownMethod();
+        this.props[methodName] ? this.props[methodName](...args) : null;
+      };
     }
-  }
-}
 
-for (var methodName in bindMethodNames) {
-  HOC[methodName] = function(args) {
-    this.props[methodName] ? this.props[methodName](...args) : null;
+    WrappedComponent[methodName] = compoaseFunction(methodName);
   }
+  return WrappedComponent;
 }
 
 export default HOC;
